b3450415b981b6a0c77f100aed978284a753bf59
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-04-16 23:23:01 +0700

    fix : update statistics - support mood

--- src/Utility/Statistics.py
+++ src/Utility/Statistics.py
@@ -4,15 +4,19 @@
 
 import matplotlib.animation as ani
 import matplotlib.pyplot as plt
-from functools import partial
+import pandas as pd
 
 class Statistics:
+    
     # CONSTRUCTOR
     # Konstruktor default
     # Masukan tipe : Mood / Sleep
-    def __init__ (self, data, tipe):
-        self.data = data
+    def __init__ (self, filename, tipe):
+        self.filename = filename
         self.tipe = tipe
+        df = pd.read_csv(self.filename, delimiter=',', header='infer')
+        self.df = df.tail(7) # Mengambil 7 data terakhir
+        self.df.set_index('tanggal', inplace=True) # Menjadikan tanggal sebagai indeks
     
     # GETTER
     # Mengembalikan atribut tipe
@@ -20,29 +24,25 @@ class Statistics:
         return self.tipe
     
     # PEMBUATAN STATISTIK
-    def createChart (self, i=int):
+    def buildmebarchart(self, i=int):
+        # Pilihan warna
         color = ['red', 'green', 'blue', 'orange']
+        plt.legend(self.df.columns)
+        p = plt.plot(self.df[:i].index, self.df[:i]) # Melakukan pemrosesan hingga nilai ke-i
         if (self.tipe == "Mood"):
-            plt.legend(["Rate", "Relax Level", "Energy Level"], loc = 0)
-            p = plt.plot(self.data[:i].index, self.data[:i].values) #note it only returns the dataset, up to the point i
-            for j in range(0, 3):
-                p[j].set_color(color[j]) # Mengubah warna setiap kurva
+            for i in range (3):
+                p[i].set_color(color[i]) # Mengubah warna setiap atribut data
         elif (self.tipe == "Sleep"):
-            plt.legend(["Rate", "Durasi Tidur"], loc = 0)
-            p = plt.plot(self.data[:i].index, self.data[:i].values) #note it only returns the dataset, up to the point i
-            for j in range(0, 2):
-                p[j].set_color(color[j]) # Mengubah warna setiap kurva
+            for i in range (2):
+                p[i].set_color(color[i]) # Mengubah warna setiap atribut data
         
     def generateStatistics (self):
+        # Membuat sebuah figur
         fig = plt.figure()
-        plt.xticks(rotation = 45, ha="right", rotation_mode="anchor") # Melakukan pemutaran atribut tanggal
-        plt.subplots_adjust(bottom = 0.2, top = 0.9) # Memastikan tanggal "muat" diposisiskan
-        if (self.tipe == "Mood"):
-            plt.ylabel('Skala')
-            plt.xlabel('Tanggal')
-        elif (self.tipe == "Sleep"):
-            plt.ylabel('Lama Waktu')
-            plt.xlabel('Tanggal')
-            
-        animator = ani.FuncAnimation(fig, partial(Statistics.createChart, self), interval = 100)
+        plt.xticks(rotation = 45, ha = "right", rotation_mode = "anchor") # Merotasi nilai tanggal 45 deg
+        plt.subplots_adjust(bottom = 0.2, top = 0.9) # Memastikan nilai tanggal tidak terpotong
+        plt.ylabel('Values')
+        plt.xlabel('Dates')
+        
+        animator = ani.FuncAnimation(fig, self.buildmebarchart, interval = 100)
         plt.show()
