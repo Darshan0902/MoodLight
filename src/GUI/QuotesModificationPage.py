89dea4521d6584d23b7d9713dc0319bf83dae5eb
Nat10k <110515021+Nat10k@users.noreply.github.com>
2023-04-19 18:46:53 +0700

    feat : beautify quotes page

--- src/GUI/QuotesModificationPage.py
+++ src/GUI/QuotesModificationPage.py
@@ -19,30 +19,30 @@ class QuotesModificationPage(CTk):
         self.quotes_controller = QuoteModificationController()
 
         # Menciptakan frame pertama tempat mengubah data quotes
-        self.quotes_modif_frame = CTkFrame(self.master, corner_radius=0, fg_color="transparent")
+        self.quotes_modif_frame = CTkFrame(self.master, corner_radius=0, fg_color="#568ea6")
         self.quotes_image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "images")
         
         self.quotes_image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "images")
         self.quotes_logo_image = CTkImage(Image.open(os.path.join(self.quotes_image_path, "logo.png")), size=(150,30))
         self.quotes_logo_image_label = CTkLabel(self.quotes_modif_frame, text="", image=self.quotes_logo_image)
-        self.quotes_logo_image_label.grid(row=0, column=0, padx=(0,206), pady=(10,0), sticky="ne")
+        self.quotes_logo_image_label.grid(row=0, column=0, padx=(0,100), pady=(10,0), sticky="ne")
 
-        self.quotes_list_frame = CTkScrollableFrame(self.quotes_modif_frame,width=700,height=500,corner_radius=0,fg_color="transparent")
-        self.quotes_list_frame.grid(row=1,column=0,padx=400,pady=30)
+        self.quotes_list_frame = CTkScrollableFrame(self.quotes_modif_frame,width=900,height=500,corner_radius=0,fg_color="#00b4d8")
+        self.quotes_list_frame.grid(row=1,column=0,padx=250,pady=(30,20))
         
         self.quotes_textboxes = []
         for i in range(len(self.quotes_controller.data)) :
-            self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=650,height=30))
+            self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=890,height=40,font=CTkFont(size=18)))
             self.quotes_textboxes[i].insert("0.0", self.quotes_controller.data[i].getContent() + " ~ " + self.quotes_controller.data[i].getAuthor())
             self.quotes_textboxes[i].grid(row=i,column=0,padx=10,pady=10)
         
-        self.format_label = CTkLabel(self.quotes_modif_frame,text="Quote format : <Quote> ~ <Author>")
+        self.format_label = CTkLabel(self.quotes_modif_frame,text="Quote format : <Quote> ~ <Author>", text_color="white", font=CTkFont(size=20,weight="bold"))
         self.format_label.grid(row=2,column=0,padx=100,pady=5)
 
-        self.quotes_add = CTkButton(self.quotes_modif_frame, text="Add Quotes", command=self.quotes_add_button_event)
+        self.quotes_add = CTkButton(self.quotes_modif_frame, text="Add Quotes", font=CTkFont(size=18), command=self.quotes_add_button_event, width=90, height=40, fg_color="#305f72")
         self.quotes_add.grid(row=3, column=0, padx=100, pady=5)
         
-        self.quotes_save = CTkButton(self.quotes_modif_frame, text="Save", command=self.quotes_save_button_event)
+        self.quotes_save = CTkButton(self.quotes_modif_frame, text="Save", font=CTkFont(size=18), command=self.quotes_save_button_event, width=90, height=40, fg_color="#305f72")
         self.quotes_save.grid(row=4, column=0, padx=100, pady=5)
 
     # GETTER
@@ -70,12 +70,12 @@ class QuotesModificationPage(CTk):
         self.quotes_textboxes.clear()
         
         for i in range(len(self.quotes_controller.data)) :
-            self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=650,height=30))
+            self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=890,height=40,font=CTkFont(size=18)))
             self.quotes_textboxes[i].insert("0.0", self.quotes_controller.data[i].getContent() + " ~ " + self.quotes_controller.data[i].getAuthor())
             self.quotes_textboxes[i].grid(row=i,column=0,padx=10,pady=10)
     
     # Menambahkan textbox baru
     def quotes_add_button_event(self):
-        self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=650,height=30,))
+        self.quotes_textboxes.append(CTkTextbox(self.quotes_list_frame,width=890,height=40,font=CTkFont(size=18)))
         self.quotes_textboxes[len(self.quotes_textboxes)-1].grid(row=len(self.quotes_textboxes)-1,column=0,padx=10,pady=10)
     
